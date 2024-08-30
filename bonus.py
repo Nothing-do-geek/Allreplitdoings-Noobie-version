42c0cb7a53183a86b4299e80a3f3ba5f58b5db8e
Nothing-do-geek <157900702+Nothing-do-geek@users.noreply.github.com>
2024-08-31 00:24:01 +0300

    bonus.py

---
+++ bonus.py
@@ -0,0 +1,20 @@
+# x = 36**8 + 6**20 - 12
+# s = ''
+# while x != 0: 
+#     s += str(x % 6)
+#     x //= 6
+# s = s[::-1]
+# print(s.count("0"))
+
+#def f(x,y):
+    #if x == y:
+        #return 1
+    #if x > y:
+        #return 0
+    #return f(x + 2, y) + f(x * 2, y)
+#print(f(2, 42))
+
+#for x in range(174457,174505+1):
+  #k=0
+  #s=[]
+  #for y in range()
